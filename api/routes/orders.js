b269397ad233966d0838bca7f133d841e26a698c
Zachary Khan <zachary.khan3@gmail.com>
2018-10-02 14:22:25 -0700

    Add body-parser and CORS handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+// handle incoming GET requests to /orders
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders have been fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+    res.status(201).json({
+        message: 'Order successfully created',
+        order: order
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const orderId = req.params.orderId;
+    res.status(200).json({
+        message: 'Details for order with id: ' + orderId,
+        id: orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    const orderId = req.params.orderId;
+    res.status(200).json({
+        message: 'Deleted order with id: ' + orderId,
+        id: orderId
+    });
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 const express = require('express');
 const app = express();
-const productRoutes = require('./api/routes/products');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+// log all incoming requests
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+    next();
+});
+
+// routes to handle requests
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// handle all requests that make it beyond the scope of our API routes with error
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+// handle errors from within server
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+            code: error.status || 500
+        }
+    });
+});
 
 module.exports = app;
