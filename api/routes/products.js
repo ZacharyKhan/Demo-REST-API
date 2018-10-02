2c345d4613d328b9d32af48d2d5d79db6c4a7026
Zachary Khan <Zachary.Khan3@gmail.com>
2018-10-02 12:05:39 -0700

    Initial work

---
+++ api/routes/products.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling POST requests to /products'
+  });
+});
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'You discovered the special ID',
+      id: id
+    });
+  } else {
+    res.status(200).json({
+      message: 'You passed the following ID to the api: ' + id
+    });
+  }
+});
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'You updated the product.'
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'You deleted the product.'
+  });
+});
+
+module.exports = router;
